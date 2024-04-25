e4e0170d6739f7aadb8f5819b43979703a337b14
Aaleeya <shaikhaaleeya17@gmail.com>
2024-04-24 22:40:37 -0400

    updated files

---
+++ src/exe_String/Lab05.java
@@ -0,0 +1,12 @@
+package exe_String;
+
+public class Lab05 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int max = a < b ? b:a;
+        int min = a > b ? b:a;
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
